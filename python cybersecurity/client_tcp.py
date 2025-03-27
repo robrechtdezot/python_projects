9ee70853bc824451073297ce616a38c6fecda22f
Wotto <woutservaes1@gmail.com>
2025-03-27 03:26:38 +0100

    hju

---
+++ python cybersecurity/client_tcp.py
@@ -0,0 +1,9 @@
+import socket
+
+clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = socket.gethostname()
+port = 444
+clientsocket.connect(('192.168.1.104', port))
+message = clientsocket.recv(1024)
+clientsocket.close()
+print(message.decode('ascii'))
--- python cybersecurity/server_tcp.py
+++ python cybersecurity/server_tcp.py
@@ -2,7 +2,9 @@ import socket
 
 
 
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+
+#host = 'ip ardes'
 host = socket.gethostname()
 port = 444
 serversocket.bind((host, port)) # bind port and host to the server
@@ -11,14 +13,10 @@ serversocket.listen(3) # listen for connections
 print ('Server is listening for connections')
 while True:
     clientsocket, address = serversocket.accept()
-    print ("Got connection from" % str(address))
+    print ('Got a connection from %s' % str(address))         #print('Received connection from ' + str(address))       
     message = 'Thank you for connecting' + "\r\n"
+    clientsocket.send(message.encode('ascii'))
     clientsocket.close()
-    
-    
-    # clientsocket.send(message.encode('ascii'))
-    
-    
     
     # data = clientsocket.recv(1024)
     # print ("Received data:", data)
